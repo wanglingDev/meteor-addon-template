7fb21c355e77f2cde15999a376af99e22026976a
root <root@localhost.localdomain>
2026-06-27 15:23:35 +0000

    fix modCompileOnly syntax

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,9 +28,7 @@ dependencies {
     mappings(loom.officialMojangMappings())
     modImplementation(libs.fabric.loader)
 
-    modCompileOnly(files("libs/meteor-client.jar")) {
-        isTransitive = false
-    }
+    modCompileOnly(files("libs/meteor-client.jar"))
     compileOnly(files("libs/orbit-0.2.4.jar"))
 }
 
