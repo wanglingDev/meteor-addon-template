d624a3d0567ddd2234b249e6c46d01e822cc4b57
root <root@localhost.localdomain>
2026-07-08 07:21:36 +0000

    switch to Yarn + Maven Meteor, fix remapJar

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,64 +8,30 @@ version = property("mod_version").toString()
 group   = property("maven_group").toString()
 base.archivesName.set(property("archives_base_name").toString())
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_25
-    targetCompatibility = JavaVersion.VERSION_25
-}
-
-loom {
-    runConfigs["client"].ideConfigGenerated(true)
-    mixin.defaultRefmapName.set("addon.refmap.json")
-}
-
-tasks.remapJar {
-    classpath.from(
-        files("repo/meteordevelopment/meteor-client/26.1.2-local/meteor-client-26.1.2-local.jar")
-    )
-}
-
-tasks.build {
-    dependsOn(tasks.jar)
-}
-
-// force Loom to search mavenLocal
-configurations.all {
-    resolutionStrategy.eachDependency {
-        if (requested.group == "meteordevelopment" && requested.name == "meteor-client") {
-            useVersion("26.1.2-local")
-        }
-    }
-}
-
 repositories {
-    maven(url = uri("${rootProject.projectDir}/repo"))
-    mavenCentral()
-    maven("https://maven.meteordev.org/releases")
-    maven("https://maven.meteordev.org/snapshots")
+    maven { url = uri("https://maven.meteordev.org/releases") }
+    maven { url = uri("https://maven.meteordev.org/snapshots") }
 }
 
 dependencies {
     minecraft(libs.minecraft)
-    mappings(loom.officialMojangMappings())
-    modImplementation(libs.fabric.loader)
-    compileOnly(files("repo/meteordevelopment/meteor-client/26.1.2-local/meteor-client-26.1.2-local.jar"))
-    compileOnly(files("libs/orbit-0.2.4.jar"))
-}
-
-afterEvaluate {
-    tasks.named<JavaCompile>("compileJava") {
-        classpath = classpath +
-            files("${rootProject.projectDir}/repo/meteordevelopment/meteor-client/26.1.2-local/meteor-client-26.1.2-local.jar") +
-            files("${rootProject.projectDir}/libs/orbit-0.2.4.jar")
-    }
+    mappings(variantOf(libs.yarn) { classifier("v2") })
+    modImplementation(libs.fabricLoader)
+    modCompileOnly(libs.meteorClient)
 }
 
 tasks.processResources {
-    val props = mapOf(
-        "version" to version,
-        "mc_version" to libs.versions.minecraft.get(),
-        "loader_version" to libs.versions.fabric.loader.get()
-    )
+    val props = mapOf("version" to version, "mc_version" to libs.versions.minecraft.get())
     inputs.properties(props)
     filesMatching("fabric.mod.json") { expand(props) }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.release = 21
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
