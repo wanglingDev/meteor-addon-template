ff9fb52391c3bed5d9353f34e974f51af6a0c0a3
root <root@localhost.localdomain>
2026-07-08 06:54:04 +0000

    disable remapJar

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ java {
 loom {
     runConfigs["client"].ideConfigGenerated(true)
     mixin.defaultRefmapName.set("addon.refmap.json")
+    remapArchives = false
 }
 
 // force Loom to search mavenLocal
